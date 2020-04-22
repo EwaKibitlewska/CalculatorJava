4b3b07f8fd9746769d33feb120ab6afcd77c0a36
unknown <malampost@gmail.com>
2020-04-22 20:38:40 +0200

    my first repository

---
+++ src/Calculate.java
@@ -0,0 +1,15 @@
+public class Calculate {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        
+        System.out.println("A + B = " + " " + calculator.add(20, 5));
+        System.out.println("A / B = " + " " + calculator.divide(20, 5));
+        System.out.println("A - B = " + " " + calculator.subtract(20, 5));
+        System.out.println("A * B = " + " " + calculator.multiply(20, 5));
+
+
+
+
+    }
+}
